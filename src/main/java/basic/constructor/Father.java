bc5153f1dd34377d5b45f185c26963889731adda
xMustang <xMustang@foxmail.com>
2019-08-06 09:08:56 +0800

    修复冲突

--- src/main/java/basic/constructor/Father.java
+++ src/main/java/basic/constructor/Father.java
@@ -1,17 +1,21 @@
 package basic.constructor;
 
 class Father {
-    public Father() {
-        System.out.println("父类构造器执行");
-        i = 3;
-    }
-    {
-        System.out.println("父类初始化块");
-        i = 2;
-    }
-    public int initI() {
-        System.out.println("父类成员变量定义时赋值（显式初始化）");
-        return 1;
-    }
-    private int i = initI();
+	public Father() {
+		System.out.println("父类构造器执行");
+		i = 3;
+	}
+
+	{
+		System.out.println("父类初始化块");
+		i = 2;
+	}
+
+	public int initI() {
+		System.out.println("父类成员变量定义时赋值（显式初始化）");
+		return 1;
+	}
+
+	private int i = initI();
+
 }
