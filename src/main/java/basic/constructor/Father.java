3b5e5291689aac9c8bca208165781fa66c00b57d
xMustang <xMustang@foxmail.com>
2019-08-06 10:56:11 +0800

    添加已笔记

--- src/main/java/basic/constructor/Father.java
+++ src/main/java/basic/constructor/Father.java
@@ -1,5 +1,8 @@
 package basic.constructor;
 
+/**
+ * 代码已被笔记使用
+ */
 class Father {
 	public Father() {
 		System.out.println("父类构造器执行");
--- src/main/java/basic/constructor/Son.java
+++ src/main/java/basic/constructor/Son.java
@@ -2,27 +2,29 @@ package basic.constructor;
 
 /**
  * 构造器、初始化块的执行顺序
+ * <p>
+ * 代码已被笔记使用
  */
 public class Son extends Father {
 
-    public Son() {
-        System.out.println("子类构造器执行");
-        j = 3;
-    }
+	public Son() {
+		System.out.println("子类构造器执行");
+		j = 3;
+	}
 
-    {
-        System.out.println("子类初始化块");
-        j = 2;
-    }
+	{
+		System.out.println("子类初始化块");
+		j = 2;
+	}
 
-    private int j = initJ();
+	private int j = initJ();
 
-    public int initJ() {
-        System.out.println("子类成员变量定义时赋值（显式初始化）");
-        return 1;
-    }
+	public int initJ() {
+		System.out.println("子类成员变量定义时赋值（显式初始化）");
+		return 1;
+	}
 
-    public static void main(String[] args) {
-        new Son();
-    }
+	public static void main(String[] args) {
+		new Son();
+	}
 }
