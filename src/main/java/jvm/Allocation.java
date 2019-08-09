d336673124e1823344c86e798c4ddf59a5268a10
xMustang <xMustang@foxmail.com>
2019-08-09 08:35:55 +0800

    部分修改

--- src/main/java/jvm/Allocation.java
+++ src/main/java/jvm/Allocation.java
@@ -5,17 +5,17 @@ package jvm;
  * -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
  */
 public class Allocation {
-	private static final int _1MB = 1024 * 1024;
+    private static final int _1MB = 1024 * 1024;
 
-	public static void testAllocation() {
-		byte[] allocation1, allocation2, allocation3, allocation4;
-		allocation1 = new byte[2 * _1MB];
-		allocation2 = new byte[2 * _1MB];
-		allocation3 = new byte[2 * _1MB];
-		allocation4 = new byte[4 * _1MB];
-	}
+    public static void testAllocation() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB];
+    }
 
-	public static void main(String[] args) {
-		testAllocation();
-	}
+    public static void main(String[] args) {
+        testAllocation();
+    }
 }
--- src/main/java/jvm/TenuringThreshold2.java
+++ src/main/java/jvm/TenuringThreshold2.java
@@ -7,22 +7,22 @@ package jvm;
  * -XX:+PrintTenuringDistribution
  */
 public class TenuringThreshold2 {
-	private static final int _1MB = 1024 * 1024;
+    private static final int _1MB = 1024 * 1024;
 
-	@SuppressWarnings("unused")
-	public static void testTenuringThreshold() {
-		byte[] allocation1, allocation2, allocation3, allocation4;
-		allocation1 = new byte[_1MB / 4];
-		// allocation1+allocation2大于Survivor空间一半
-		allocation2 = new byte[_1MB / 4];
-		allocation3 = new byte[4 * _1MB];
-		allocation4 = new byte[4 * _1MB];
+    @SuppressWarnings("unused")
+    public static void testTenuringThreshold() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[_1MB / 4];
+        // allocation1+allocation2大于Survivor空间一半
+        allocation2 = new byte[_1MB / 4];
+        allocation3 = new byte[4 * _1MB];
+        allocation4 = new byte[4 * _1MB];
 
-		allocation4 = null;
-		allocation4 = new byte[4 * _1MB];
-	}
+        allocation4 = null;
+        allocation4 = new byte[4 * _1MB];
+    }
 
-	public static void main(String[] args) {
-		testTenuringThreshold();
-	}
+    public static void main(String[] args) {
+        testTenuringThreshold();
+    }
 }
