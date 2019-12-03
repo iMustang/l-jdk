e69c0c71e6472d6de8906b9462a7804ce9d66e08
xMustang <xMustang@foxmail.com>
2019-12-03 17:17:28 +0800

    ConcurrentHashMap

---
+++ src/main/java/container/map/ConcurrentHashMapDemo.java
@@ -0,0 +1,19 @@
+package container.map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * ConcurrentHashMapDemo
+ * <p>
+ * ConcurrentHashMap的Key、Value不能为null
+ *
+ * @author: xMustang
+ * @since: 1.0
+ */
+public class ConcurrentHashMapDemo {
+	public static void main(String[] args) {
+		ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+		map.put(null, "1");
+		System.out.println(map);
+	}
+}
