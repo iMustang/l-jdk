72047812c2c5d1143a14d6d47f11393cee2bbdc0
xMustang <xMustang@foxmail.com>
2019-12-19 18:24:37 +0800

    异常

---
+++ src/main/java/basic/exception/Main.java
@@ -0,0 +1,14 @@
+package basic.exception;
+
+/**
+ * Main
+ *
+ * @author: xMustang
+ * @since: 1.0
+ */
+public class Main {
+	public static void main(String[] args) {
+		NullPointerException ex = new NullPointerException();
+		System.out.println(ex.getMessage());
+	}
+}
---
+++ src/main/java/container/Main.java
@@ -0,0 +1,21 @@
+package container;
+
+/**
+ * Main
+ *
+ * @author: xMustang
+ * @since: 1.0
+ */
+public class Main {
+	public static void main(String[] args) {
+		String str = null;
+
+		child();
+
+		System.out.println("456");
+	}
+
+	public static void child() {
+		System.out.println("123");
+	}
+}
