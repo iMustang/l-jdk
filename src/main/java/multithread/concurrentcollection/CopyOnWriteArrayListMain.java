d916646954ab60a722ae1ce8eed6487736660902
xMustang <xMustang@foxmail.com>
2020-01-10 13:35:57 +0800

    BlockingQueue

---
+++ src/main/java/multithread/concurrentcollection/CopyOnWriteArrayListMain.java
@@ -0,0 +1,16 @@
+package multithread.concurrentcollection;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * CopyOnWriteArrayListMain
+ *
+ * @author xMustang
+ * @since 1.0
+ */
+public class CopyOnWriteArrayListMain {
+    public static void main(String[] args) {
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+
+    }
+}
